3784d0700028d95859ef4d1af2bd559a53e957dc
ASHLEYANDRES <aandres7@hawaii.edu>
2021-02-22 20:10:40 -1000

    LAB

---
+++ Example.js
@@ -0,0 +1,12 @@
+s = 0;
+
+x = 1;
+
+while (x <= 3) {
+
+    s = s + x;                 
+
+    x = x + 1; //count up
+
+}
+console.log(x,s);
---
+++ Lab 7/SmartPhoneProducts1_4/junk.js
@@ -0,0 +1,4 @@
+age =  20;
+fav_num = 7;
+day_of_birth = 6;
+month_of_birth = 5;
