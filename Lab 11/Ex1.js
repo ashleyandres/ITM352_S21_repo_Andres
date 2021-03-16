fa67f4a3efdfc3672122c32cb477e987468fa157
ASHLEYANDRES <aandres7@hawaii.edu>
2021-03-16 08:48:13 -1000

    b

--- Lab 11/Ex1.js
+++ Lab 11/Ex1.js
@@ -1,3 +1,15 @@
+function isNonNegInt(q){
+    errors = []; // assume no errors at first
+if(Number(q) != q,returnErrors ) errors.push('Not a number!'); // Check if string is a number value
+if(q < 0) errors.push('Negative value!'); // Check if it is non-negative
+if(parseInt(q) != q) errors.push('Not an integer!'); // Check that it is an integer
+returnErrors ? errors : (errors.length == 0).  (errors.length == 0)
+}
+
+
 attributes  =  "Ashley;21;MIS.";
 parts = attributes.split(';');
-console.log(parts);
+for (part of parts){
+    console.log(isNonNegInt);
+}
+//console.log(parts);
