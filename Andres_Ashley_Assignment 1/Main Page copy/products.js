60fdd6ec479043a7a46bc6bec7e87b37f60550dc
ASHLEYANDRES <aandres7@hawaii.edu>
2021-03-23 23:44:32 -1000

    Lab +A1

---
+++ Andres_Ashley_Assignment 1/Main Page copy/products.js
@@ -0,0 +1,43 @@
+products = [
+
+    //product1
+{
+  name : "Black/Gold Bracelet",
+  price : 25.00,
+  image : "https://images-na.ssl-images-amazon.com/images/I/71eh8QANwIL._AC_UY695_.jpg"
+    },
+
+
+//product2
+ {
+ name :"Green Stone Bracelet",
+ price: 15.00,
+ image : "https://images-na.ssl-images-amazon.com/images/I/61bUpCCIFWL._AC_UY695_.jpg"
+},
+
+
+//product3
+{
+name : "White/Gold Bracelet",
+price : 25.00,
+image : "https://images-na.ssl-images-amazon.com/images/I/61sraQV82kL._AC_UY695_.jpg"
+},
+
+
+//product4
+ {
+name : "Purple Bracelet",
+price : 10.00,
+image : "https://images-na.ssl-images-amazon.com/images/I/617vEEak3xL._AC_UY695_.jpg"
+},
+
+
+//product5
+{
+name : "Pink Bracelet",
+price : 10.00,
+image : "https://m.media-amazon.com/images/I/61gFZ1EfedL._AC_SY695._SX._UX._SY._UY_.jpg"
+}
+
+
+    ];
